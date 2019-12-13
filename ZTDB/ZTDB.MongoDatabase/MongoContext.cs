4ee8f4778c4071764af7c2bbe55c6d6d78993194
ArekerA <Arek.ludwikowski@hotmail.com>
2019-12-14 00:05:45 +0100

    Adding projects and nuget packages

---
+++ ZTDB/ZTDB.MongoDatabase/MongoContext.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Driver;
+using System;
+
+namespace ZTDB.MongoDatabase
+{
+    public class MongoContext
+    {
+        public MongoClient Client { get; set; }
+        public MongoContext()
+        {
+            Client = new MongoClient();
+        }
+    }
+}
---
+++ ZTDB/ZTDB.SQLDatabase/SQLContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace ZTDB.SQLDatabase
+{
+    public class SQLContext : DbContext
+    {
+    }
+}
