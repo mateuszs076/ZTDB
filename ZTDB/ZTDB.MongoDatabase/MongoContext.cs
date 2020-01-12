7b9be6cdf668093ade0eacfe4f27c89d681a9ffb
ArekerA <Arek.ludwikowski@hotmail.com>
2020-01-12 16:05:11 +0100

    Dodanie wstępnej obsługi mongodb

--- ZTDB/ZTDB.MongoDatabase/MongoContext.cs
+++ ZTDB/ZTDB.MongoDatabase/MongoContext.cs
@@ -1,14 +1,29 @@
 ﻿using MongoDB.Driver;
 using System;
+using System.Collections.Generic;
+using ZTDB.MongoDatabase.Models;
 
 namespace ZTDB.MongoDatabase
 {
     public class MongoContext
     {
-        public MongoClient Client { get; set; }
+        private readonly IMongoCollection<Flight> _flights;
         public MongoContext()
         {
-            Client = new MongoClient();
+            var client = new MongoClient("mongodb://localhost:27017");
+            var database = client.GetDatabase("flights");
+            _flights = database.GetCollection<Flight>("flights");
         }
+        /// <summary>
+        /// Pobiera wszystkie loty
+        /// </summary>
+        /// <returns></returns>
+        public List<Flight> Get() =>
+          _flights.Find(a => true).ToList();
+        public List<Flight> Get(int limit) =>
+          _flights.Find(a => true).SortBy(a => a.Id).Limit(limit).ToList();
+
+        public Flight Get(string id) =>
+            _flights.Find<Flight>(a => a.Id == id).FirstOrDefault();
     }
 }
